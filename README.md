ac5a2aa1acd6e742ae0d53e2d6777637b859952a
Mohd Faiz Hasim <faizhasim@gmail.com>
2017-02-28 16:48:21 +0800

    [LD-3909] Improve CLI

---
+++ README.md
@@ -0,0 +1,9 @@
+# serverless-dynamic-dynamodb
+
+> Provision dynamic dynamodb throughput using serverless framework or CLI.
+
+## CLI
+
+    npm install -g serverless-dynamic-dynamodb
+    
+    provision-dynamodb --help
--- cli.js
+++ cli.js
@@ -1,4 +1,43 @@
-import config from './lib/config';
-import {processConfigTables} from './lib';
+// const argv = require('yargs').argv;
+// const {processConfigTables} = require('./dist');
 
-processConfigTables(config.tables);
+// import {processConfigTables} from './lib';
+//
+// processConfigTables(config.tables);
+
+const fs = require('fs');
+const yargs = require('yargs');
+const {processConfigTables} = require('./dist');
+
+const parseJson = jsonstring => {
+  try {
+    return JSON.parse(jsonstring);
+  } catch (err) {
+    throw new Error(`Invalid JSON structure. ${err.message}.`);
+  }
+};
+
+const provisionFromJson = ({jsonstring}) => {
+  const json = parseJson(jsonstring);
+  processConfigTables(json)
+    .then(() => {})
+    .catch(err => {
+      throw err
+    });
+};
+
+const provisionFromJsonFile = ({filepath}) => {
+  if (!fs.existsSync(filepath)) {
+    throw new Error(`Invalid path "${filepath}" specified.`);
+  }
+  const content = fs.readFileSync(filepath);
+  provisionFromJson({jsonstring: String(content)});
+};
+
+yargs
+  .usage('$0 <cmd> [args]')
+  .command('json [jsonstring]', 'Run provisioner based on JSON definition', {}, provisionFromJson)
+  .command('jsonfile [filepath]', 'Run provisioner based on JSON file', {}, provisionFromJsonFile)
+  .demandCommand(1, 'Provisioner command not specified.')
+  .help()
+  .argv;
